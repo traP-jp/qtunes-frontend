e08005f074b93b507b2aff4ed13a04be1aa590c0
itt828 <ishikzm0828@gmail.com>
2021-06-17 00:34:36 +0900

    a

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,17 +1,13 @@
 <template>
   <div>This is Top</div>
-  <aaaaalist />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import aaaaalist from '../components/aaaaalist.vue'
 
 export default defineComponent({
   name: 'Top',
-  components: {
-    aaaaalist,
-  },
+  components: {},
   setup() {},
 })
 </script>
