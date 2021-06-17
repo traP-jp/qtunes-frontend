733db9a5dc90f2b76ef04d8c3feeb14a4efa9aad
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-06-18 00:53:11 +0900

    ✨ openapi generator 対応

---
+++ scripts/generate-apis.js
@@ -0,0 +1,38 @@
+// https://git.trap.jp/hackason20_winter_2/CustomTheme-Client/src/branch/master/scripts/generate-apis.js
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+const fs = require('fs').promises
+const path = require('path')
+const { exec } = require('child_process')
+const { promisify } = require('util')
+const execPromise = promisify(exec)
+const addApis = require('./add-apis')
+
+const SWAGGER_PATH =
+  'http://main.back-end.hackathon21_spring_02.trap.show/openapi/swagger.yaml' // TODO: cname ついたら変える
+const GENERATED_DIR = 'src/lib/apis/generated'
+
+const npx = process.platform === 'win32' ? 'npx.cmd' : 'npx'
+
+const generateCmd = [
+  npx,
+  'openapi-generator-cli',
+  'generate',
+  '-g',
+  'typescript-axios',
+  '-i',
+  SWAGGER_PATH,
+  '-o',
+  GENERATED_DIR,
+]
+
+;(async () => {
+  await fs.mkdir(path.resolve(__dirname, '../', GENERATED_DIR), {
+    recursive: true,
+  })
+
+  await execPromise(generateCmd.join(' '))
+
+  // generate Apis class
+  await addApis(GENERATED_DIR)
+})()
