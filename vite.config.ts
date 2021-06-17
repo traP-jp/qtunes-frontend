227086d92acbc0c318293c1716bf871d908ebd33
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-06-18 03:21:20 +0900

    ♻️ `/@` で root を指せるように

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+const srcPath = path.resolve(__dirname, 'src').replace(/\\/g, '/')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '/@': srcPath,
+    },
+  },
   plugins: [vue()],
 })
