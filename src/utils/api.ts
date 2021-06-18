faf200347e80e7d9a3d4c4300edb770046eaa38b
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-06-18 22:45:37 +0900

    🎉 api 叩けるようになった (はず)

---
+++ src/utils/api.ts
@@ -0,0 +1,79 @@
+import axios, { AxiosPromise } from 'axios'
+import { Code, Composer, ModelFile, User, UserMe } from '../lib/apis/generated'
+
+export const traQBaseURL = 'https://q.trap.jp/api/v3'
+// export const api = new Apis(
+//   new Configuration({
+//     basePath: '/api',
+//   })
+// )
+export class Apis {
+  getUsers(): AxiosPromise<User[]> {
+    return axios.get('/api/users')
+  }
+
+  getUser(id: string): AxiosPromise<User> {
+    return axios.get(`/api/users/${id}`)
+  }
+
+  getMe(): AxiosPromise<UserMe> {
+    return axios.get('/api/users/me')
+  }
+
+  getComposers(): AxiosPromise<Composer[]> {
+    return axios.get('/api/composers')
+  }
+
+  getComposer(id: string): AxiosPromise<Composer> {
+    return axios.get(`/api/composers/${id}`)
+  }
+
+  getComposerFiles(id: string): AxiosPromise<ModelFile[]> {
+    return axios.get(`/api/${id}`)
+  }
+
+  getFiles(): AxiosPromise<ModelFile[]> {
+    return axios.get('/api/files')
+  }
+
+  getFile(id: string): AxiosPromise<ModelFile> {
+    return axios.get(`/api/${id}`)
+  }
+
+  putFileFavorite(id: string, favorite: boolean): AxiosPromise<void> {
+    return axios.put(`/api/${id}/favorite`, {
+      favorite,
+    })
+  }
+
+  getGeneratedCode(): AxiosPromise<Code> {
+    return axios.get('/api/oauth/generate/code')
+  }
+
+  callback(code: string): AxiosPromise<void> {
+    return axios.get(`/api/oauth/callback?code=${code}`)
+  }
+
+  postLogout(): AxiosPromise<void> {
+    return axios.post('/api/oauth/logout')
+  }
+
+  downloadFileLink(id: string): string {
+    return `/api/files/${id}/download`
+  }
+}
+export const api = new Apis()
+
+export const redirect2AuthEndpoint = async () => {
+  const oauthParam = (await api.getGeneratedCode()).data
+  const authorizationEndpointUrl = new URL(`${traQBaseURL}/oauth2/authorize`)
+
+  const searchParams = new URLSearchParams()
+  searchParams.set('response_type', oauthParam.responseType)
+  searchParams.set('client_id', oauthParam.clientID)
+  searchParams.set('code_challenge', oauthParam.codeChallenge)
+  searchParams.set('code_challenge_method', oauthParam.codeChallengeMethod)
+  authorizationEndpointUrl.search = searchParams.toString()
+
+  window.location.assign(authorizationEndpointUrl.toString())
+}
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://main.back-end.hackathon21_spring_02.trap.show/', // TODO: cname で書き換える
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   plugins: [vue()],
 })
