ddc281cbeb6ab57a8290185a4f7ab3c04c14e251
itt828 <ishikzm0828@gmail.com>
2021-06-18 13:36:51 +0900

    とりあえず

---
+++ src/components/MenuIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-tooltip effect="dark" placement="bottom" content="aaa">
+    <div :class="`el-icon-platform-eleme`" />
+  </el-tooltip>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'MenuIcon',
+  props: {
+    // name: {
+    //   type: String,
+    //   required: true,
+    // },
+    // icon: {
+    //   type: String,
+    //   required: true,
+    // },
+  },
+  setup() {},
+})
+</script>
