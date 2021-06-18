e253e1f1943ce79ee56dc2a29e6712d8403f462e
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-06-18 23:52:40 +0900

    🐛 proxy の設定変更

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,12 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://main.back-end.hackathon21_spring_02.trap.show/', // TODO: cname で書き換える
+        target:
+          process.env.NODE_ENV === 'production'
+            ? 'https://main.back-end.hackathon21_spring_02.trap.show/' // TODO: cname で書き換える
+            : 'http://localhost:3000/',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        // rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
