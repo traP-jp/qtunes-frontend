a4a4285bc58916d2482e7216b6a6b880fdea885d
itt828 <ishikzm0828@gmail.com>
2021-06-19 11:21:00 +0900

    Revert "Revert "Merge branch 'feat/audio' into itt8""
    This reverts commit ad569329178cd393a02c751478fc99b206a832b1.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,38 @@
-import { createApp } from 'vue'
+import { createApp, InjectionKey } from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
+import { createStore, Store, useStore as baseUseStore } from 'vuex'
 
-createApp(App).use(ElementPlus).use(router).mount('#app')
+interface StateType {
+  id: string
+  title: string
+  composer: string
+}
+type PayloadType = Pick<StateType, 'id' | 'title' | 'composer'>
+const key: InjectionKey<Store<StateType>> = Symbol()
+const store = createStore<StateType>({
+  state(): StateType {
+    return {
+      id: '',
+      title: '',
+      composer: '',
+    }
+  },
+  mutations: {
+    chgAudio(state: StateType, payload: PayloadType) {
+      state.id = payload.id
+      state.title = payload.title
+      state.composer = payload.composer
+    },
+  },
+  actions: {
+    chgAudio(context, payload: PayloadType) {
+      context.commit('chgAudio', payload)
+    },
+  },
+})
+export const useStore = () => baseUseStore(key)
+
+createApp(App).use(ElementPlus).use(store, key).use(router).mount('#app')
--- src/views/Top/index.vue
+++ src/views/Top/index.vue
@@ -1,14 +1,32 @@
 <template>
   <div>This is Top</div>
+  <router-link to="/files">to files</router-link>
+  <el-input v-model="id" />
+  <el-button @click="chgId">chg ID</el-button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import { useStore } from '../../main'
 
 export default defineComponent({
   name: 'Top',
   components: {},
-  setup() {},
+  setup() {
+    const store = useStore()
+    const id = ref('')
+    const chgId = () => {
+      store.dispatch('chgAudio', {
+        id: id.value,
+        title: 'sample title',
+        composer: 'takku_bobshiroshiro_titech_trap',
+      })
+    }
+    return {
+      id,
+      chgId,
+    }
+  },
 })
 </script>
 
