cf86755a2605c78712f32c80cafaac2e9a77baf5
itt828 <ishikzm0828@gmail.com>
2021-06-19 12:14:10 +0900

    conflict解消

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -5,15 +5,11 @@
       <!-- <el-header class="header-container"> header </el-header> -->
       <el-container>
         <el-aside class="aside-content">
-<<<<<<< HEAD
-          <CreatorsList />
-=======
           <div>
             <el-scrollbar class="scrollbar">
               <CreatorsList />
             </el-scrollbar>
           </div>
->>>>>>> 64c12e2... スクロールバー移動した
         </el-aside>
         <el-main class="main-content">
           <slot />
@@ -50,24 +46,11 @@ export default defineComponent({
     background-color: #ee9;
   }
   .aside-content {
-<<<<<<< HEAD
-    height: calc(100vh - 138px);
-  }
-}
-
-.fixed-footer {
-  position: fixed;
-  bottom: 0;
-  // height: auto;
-  width: 100vw;
-  background-color: white;
-=======
     height: 100vh;
     overflow: auto;
     .scrollbar {
       height: 100%;
     }
   }
->>>>>>> 64c12e2... スクロールバー移動した
 }
 </style>
