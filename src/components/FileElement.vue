3d857ab3f362243e8c45b8728bd6e1b1ee153c09
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-06-20 07:20:05 +0900

    🐛 途切れるのを修正

--- src/components/FileElement.vue
+++ src/components/FileElement.vue
@@ -129,10 +129,12 @@ export default defineComponent({
       text-overflow: ellipsis;
     }
     .sound-title {
-      font-size: 1.2rem;
+      font-size: 16px;
+      line-height: 20px;
     }
     .sound-composer {
-      font-size: 0.8rem;
+      font-size: 12px;
+      line-height: 16px;
       color: #909399;
       text-decoration-line: none;
     }
