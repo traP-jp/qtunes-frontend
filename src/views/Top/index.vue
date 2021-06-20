605fbde50cd365934936cc241fe61d8501265e66
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-06-20 12:46:48 +0900

    🎉 Top 追加

--- src/views/Top/index.vue
+++ src/views/Top/index.vue
@@ -1,11 +1,43 @@
 <template>
-  <div>This is Top</div>
-  <router-link to="/files">to files</router-link>
-  <el-button @click="chgId">chg file</el-button>
+  <div>
+    <div class="title-content">
+      <i class="el-icon-refresh-right title-icon" />
+      Random
+    </div>
+    <el-card class="random-container" @click="playRandom">
+      <el-button plain circle class="big-icon-button random-content">
+        <i class="el-icon-video-play big-icon" />
+      </el-button>
+    </el-card>
+  </div>
+  <div v-if="favorites !== null && favorites.length > 0">
+    <div class="title-content">
+      <i class="el-icon-refresh-right title-icon" />
+      Favorites
+    </div>
+    <div class="fav-all-container">
+      <el-card v-for="audio in favorites" :key="audio.id" class="fav-container">
+        <el-image
+          :src="`https://q.trap.jp/api/1.0/public/icon/${audio.composer_name}`"
+          class="fav-image"
+          @click="playAudio(audio.id)"
+        />
+        <div class="sound-title">
+          {{ audio.title }}
+        </div>
+        <router-link :to="`/users/${audio.composer_name}`">
+          <div class="sound-composer">
+            {{ audio.composer_name }}
+          </div>
+        </router-link>
+      </el-card>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref, Ref } from 'vue'
+import { ModelFile } from '../../lib/apis/generated'
 import { useStore } from '../../main'
 import { api } from '../../utils/api'
 
@@ -14,7 +46,17 @@ export default defineComponent({
   components: {},
   setup() {
     const store = useStore()
-    const chgId = async () => {
+    const favorites: Ref<ModelFile[] | null> = ref(null)
+    const fetchFavorites = async () => {
+      try {
+        const { data } = await api.getMeFavorite()
+        favorites.value = data
+      } catch (err) {
+        console.error(err)
+      }
+    }
+    fetchFavorites()
+    const playRandom = async () => {
       const { data } = await api.getFileRandom()
       store.dispatch('chgAudio', {
         id: data.id,
@@ -23,11 +65,97 @@ export default defineComponent({
         isFav: data.is_favorite_by_me,
       })
     }
+    const playAudio = async (id: string) => {
+      const { data } = await api.getFile(id)
+      store.dispatch('chgAudio', {
+        id: data.id,
+        title: data.title,
+        composer: data.composer_name,
+        isFav: data.is_favorite_by_me,
+      })
+    }
     return {
-      chgId,
+      playRandom,
+      playAudio,
+      favorites,
     }
   },
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.random-container {
+  height: 200px;
+  min-height: 200px;
+  width: min(100%, 480px);
+  min-width: min(100%, 480px);
+  position: relative;
+  .random-content {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    margin: auto;
+    height: 48px;
+    width: 48px;
+  }
+}
+.big-icon-button {
+  padding: 0 !important;
+  border: 0 !important;
+  min-height: 0 !important;
+  margin: {
+    top: auto;
+    bottom: auto;
+  }
+  & > span > i {
+    font-size: 48px;
+  }
+}
+.title-content {
+  margin-bottom: 18px;
+  font-weight: bold;
+  line-height: 24px;
+  font-size: 18px;
+  .title-icon {
+    font-size: 18px;
+    margin-right: 8px;
+  }
+}
+.fav-all-container {
+  display: flex;
+  overflow: hidden;
+  .fav-container {
+    width: 160px;
+    height: 200px;
+    flex-grow: 0;
+    flex-shrink: 0;
+    margin-right: 16px;
+    .fav-image:hover {
+      opacity: 0.8;
+      transition: 0.3s;
+    }
+    & a {
+      text-decoration-line: none;
+    }
+  }
+  .sound-title,
+  .sound-composer {
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+  }
+  .sound-title {
+    font-size: 16px;
+    line-height: 20px;
+    margin-top: 8px;
+  }
+  .sound-composer {
+    font-size: 12px;
+    line-height: 16px;
+    color: #909399;
+    text-decoration-line: none;
+  }
+}
+</style>
