65aff1fdcaf63388b98834676bf1f168bc419c07
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2021-06-21 21:19:52 +0900

    ♻️ BigIconButton コンポーネントを追加

---
+++ src/components/BigIconButton.vue
@@ -0,0 +1,54 @@
+<template>
+  <el-button
+    class="big-icon-button"
+    plain
+    circle
+    @click="$emit('click', $event)"
+  >
+    <i :class="`${icon} big-icon`" />
+  </el-button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'BigIconButton',
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+  emits: ['click'],
+  setup() {},
+})
+</script>
+
+<style lang="scss" scoped>
+.big-icon-button {
+  padding: 0 !important;
+  border: 0 !important;
+  min-height: 0 !important;
+  height: 36px;
+  width: 36px;
+  & > span > i {
+    font-size: 48px;
+  }
+
+  &:hover.el-button--default,
+  &:focus.el-button--default {
+    .fav-icon {
+      color: #e6a23c !important;
+    }
+  }
+  &.el-button--warning {
+    background-color: transparent !important;
+  }
+  &:hover.el-button--warning,
+  &:focus.el-button--warning {
+    background-color: transparent !important;
+    color: #e6a23c !important;
+  }
+}
+</style>
